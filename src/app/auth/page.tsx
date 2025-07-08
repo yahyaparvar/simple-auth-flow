6d0ff1752161fa4a09c764b245b001d6f4ec3d16
yahyaparvar <yahyaparvar1@gmail.com>
2025-07-08 15:50:32 +0330

    added the auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,73 @@
+"use client";
+
+import { FormikHelpers, useFormik } from "formik";
+import * as Yup from "yup";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+import styles from "./Auth.module.scss";
+
+const validationSchema = Yup.object({
+  phone: Yup.string()
+    .matches(/^09\d{9}$/, "شماره تلفن معتبر نیست")
+    .required("شماره تلفن الزامی است"),
+});
+
+interface FormValues {
+  phone: string;
+}
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      phone: "",
+    },
+    validationSchema,
+    onSubmit: async (
+      values: FormValues,
+      { setSubmitting, setFieldError }: FormikHelpers<FormValues>
+    ) => {
+      try {
+        // I'm gonna call the api to get the user data
+      } catch (error) {
+        console.log(error);
+        setFieldError("phone", "خطا در ورود. لطفاً دوباره تلاش کنید.");
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.formContainer}>
+        <h1 className={styles.title}>ورود به پنل</h1>
+
+        <form onSubmit={formik.handleSubmit} noValidate>
+          <Input
+            label="شماره موبایل"
+            name="phone"
+            placeholder="مثلاً 09123456789"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.phone && formik.errors.phone
+                ? formik.errors.phone
+                : ""
+            }
+            required
+            minLength={11}
+            maxLength={11}
+          />
+
+          <Button fullWidth type="submit" disabled={formik.isSubmitting}>
+            {formik.isSubmitting ? "در حال ورود..." : "ورود"}
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
